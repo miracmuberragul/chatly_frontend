8641eb164522df39dfb2769116232cbefec282bf
Miraç Müberra Gül <miracmuberrag@gmail.com>
2025-07-29 10:21:17 +0300

    Remove unused contact screen import and add method to list all friendships.

--- chatly/lib/main.dart
+++ chatly/lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:chatly/screens/contact_screen.dart';
 import 'package:chatly/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
