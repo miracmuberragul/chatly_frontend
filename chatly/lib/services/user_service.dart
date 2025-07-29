425da4fc6e6a85484de8e64eeb4a01b3798357ec
dilarayyilmaz <zylmazdilara@gmail.com>
2025-07-29 14:42:38 +0300

    feat: add methods to update user profile photo, username, and change password

--- chatly/lib/services/user_service.dart
+++ chatly/lib/services/user_service.dart
@@ -1,11 +1,13 @@
+// user_service.dart
 import 'dart:developer';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Firebase Authentication için eklendi
 
 class UserService {
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  final CollectionReference _usersCollection = FirebaseFirestore.instance
-      .collection('users');
+  final CollectionReference _usersCollection = FirebaseFirestore.instance.collection('users');
+  final FirebaseAuth _auth = FirebaseAuth.instance; // Firebase Auth instance'ı eklendi
 
   /// Creates a new user document in Firestore if it doesn't already exist.
   Future<void> createUser(UserModel user) async {
@@ -50,5 +52,60 @@ class UserService {
         return UserModel.fromJson(doc.data() as Map<String, dynamic>);
       }).toList();
     });
+  }
+
+  /// Updates the user's profile photo URL.
+  /// If newPhotoUrl is null, it removes the existing profile photo URL.
+  Future<void> updateUserProfilePhoto(
+    String userId,
+    String? newPhotoUrl,
+  ) async {
+    try {
+      await _usersCollection.doc(userId).update({
+        'profilePhotoUrl': newPhotoUrl,
+      });
+      log('Profile photo updated successfully for user ID: $userId');
+    } catch (e) {
+      log('Error updating profile photo: $e');
+      rethrow;
+    }
+  }
+
+  /// Updates the user's username.
+  Future<void> updateUsername(String userId, String newUsername) async {
+    try {
+      await _usersCollection.doc(userId).update({'username': newUsername});
+      log('Username updated successfully for user ID: $userId');
+    } catch (e) {
+      log('Error updating username: $e');
+      rethrow;
+    }
+  }
+
+  /// Changes the user's password using Firebase Authentication.
+  /// This requires the user to be recently authenticated.
+  Future<void> changePassword(String newPassword) async {
+    try {
+      final user = _auth.currentUser;
+      if (user != null) {
+        await user.updatePassword(newPassword);
+        log('Password changed successfully for user: ${user.uid}');
+      } else {
+        log('No authenticated user found to change password.');
+        throw Exception('User not authenticated.');
+      }
+    } catch (e) {
+      log('Error changing password: $e');
+      rethrow;
+    }
+  }
+   Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+      log('User signed out successfully.');
+    } catch (e) {
+      log('Error signing out: $e');
+      rethrow; // Hatanın UI katmanında yakalanabilmesi için yeniden fırlat
+    }
   }
 }
