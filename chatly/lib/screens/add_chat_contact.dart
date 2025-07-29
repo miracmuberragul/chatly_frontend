2f39ce26305991831f392c922f4a036fd7c5e3fa
uniqueunp <120202013+uniqueunp@users.noreply.github.com>
2025-07-29 14:00:34 +0300

    editing chat contact page

--- chatly/lib/screens/add_chat_contact.dart
+++ chatly/lib/screens/add_chat_contact.dart
@@ -20,6 +20,7 @@ class AddChatContactPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text('Contacts'),
         centerTitle: true,
